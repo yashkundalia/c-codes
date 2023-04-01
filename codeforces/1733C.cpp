43213d41a2d5706140c6809fd51b73955b3f032e
yashkundalia <60550946+yashkundalia@users.noreply.github.com>
2023-04-01 14:42:14 +0530

    Create 1733C.cpp

---
+++ codeforces/1733C.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+#include <map>
+#include<unordered_map>
+#include <iterator>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include<utility>
+#include<climits>
+#include<stack>
+#include<set>
+#include<cmath>
+#include<deque>
+#include<numeric>
+#include<iomanip>
+
+#define ull unsigned long long
+#define ll long long
+#define NUM 998244353
+//ll mod  = 1000000007;
+using namespace std;
+
+
+
+ll binpow(ll x, ll y)
+{
+    ll res = 1ll; // Initialize result
+  
+    while (y > 0ll) {
+        // If y is odd, multiply x with result
+        if (y & 1ll)
+            res = ((res%NUM) * (x%NUM))%NUM;
+  
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = ((x%NUM) * (x%NUM))%NUM; // Change x to x^2
+    }
+    return res%NUM;
+}
+
+bool sortbysec(pair<ll,ll> &a , pair<ll,ll> &b)
+{
+    if(a.first == b.first)
+    {
+        return (a.second <= b.second);
+    }
+    return (a.first < b.first) ;
+}
+ 
+int binarysearch(vector<int> v, int find,int l, int r)
+{
+    if(find<v[l] or find > v[r]) 
+    {
+        return -1;
+    }
+    while(l<=r)
+    {
+        int mid  = l + (r-l)/2 ;
+        if(v[mid] == find) return mid;
+        if(v[mid]  > find) r = mid -1;
+        else l =mid +1;
+    }
+    return -1;
+}
+int fact(int x)
+{
+    if(x<=1) return 1;
+    int start =1;
+    int ans =1;
+    while(start<=x)
+    {
+        ans = ans*start;
+        start++;
+    }
+    return ans;
+}
+bool isvalid(ll i , ll j ,ll n,ll m)
+{
+    if(i<0 or i> n-1 or j<0 or j> m-1)
+    {
+        return false;
+    }
+    return true;
+}
+void dfs(vector<vector<int> > &adj, vector<bool> &check, int i)
+{
+    if(check[i]) return;
+    check[i] = true;
+    for(auto j : adj[i])
+    {
+        if(!check[j]) dfs(adj,check,j) ;
+    }
+    return;
+}
+vector<ll> factors(ll a, ll b)
+{
+    vector<ll> res;
+    for(ll i=1;i*i<=a;i++)
+    {
+        if(a%i ==0)
+        {
+            res.push_back(i*b);
+            res.push_back((a/i)*b);
+        }
+    }
+    return res;
+}
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<int> v;
+    for(int i=0;i<n;i++)
+    {
+        int temp;
+        cin>>temp;
+        v.push_back(temp);
+    }
+    if(n==1)
+    {
+        cout<<0<<endl;
+        return;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(v[i] <=v[i+1])
+        {
+            if(i==n-2)
+            {
+                cout<<0<<endl;
+                return;
+            }
+            continue;
+        }
+        else break;
+    }
+    int ans =0;
+    vector<pair<int,int> > pos;
+    if(v[0]!=v[n-1])
+    {
+        if((v[0] + v[n-1])%2 == 0)
+        {
+            v[0] = v[n-1];
+            ans++;
+        }
+        else
+        {
+            v[n-1] = v[0];
+            ans++;
+        }
+        pos.push_back(make_pair(1,n));
+
+    }
+    for(int i=1;i<n-1;i++)
+    {
+        if((v[0] + v[i])%2 == 1)
+        {
+            pos.push_back(make_pair(1,i+1));
+        }
+        else
+        {
+            pos.push_back(make_pair(i+1,n));
+        }
+    }
+    //cout<<"Here"<<endl;
+    cout<<pos.size()<<endl;
+    for(int i=0;i<pos.size();i++)
+    {
+        cout<<pos[i].first<<" "<<pos[i].second<<endl;
+    }
+    return;
+}
+
+
+
+
+    
+
+int main()
+{
+    
+
+    int t ;
+    cin>>t;
+    //t=1;
+    //int i=1;
+    while(t--)
+    {
+        //cout<<i<<endl;
+        solve();
+        //i++;
+    }
+    return 0;
+    
+}
