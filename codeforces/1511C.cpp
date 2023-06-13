b796eddaeee36ef3991af5d1cf8460f251573174
yashkundalia <60550946+yashkundalia@users.noreply.github.com>
2023-06-13 21:44:38 +0530

    Create 1511C.cpp

---
+++ codeforces/1511C.cpp
@@ -0,0 +1,210 @@
+#include <iostream>
+#include <map>
+#include<unordered_map>
+#include <iterator>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include<utility>
+#include<climits>
+#include<stack>
+#include<set>
+#include<cmath>
+#include<deque>
+#include<numeric>
+#include<iomanip>
+#include<queue>
+
+#define ull unsigned long long
+#define ll long long
+#define NUM 1000000007
+//ll mod  = 1000000007;
+using namespace std;
+
+
+
+ll binpow(ll x, ll y)
+{
+    ll res = 1ll; // Initialize result
+  
+    while (y > 0ll) {
+        // If y is odd, multiply x with result
+        if (y & 1ll)
+            res = ((res%NUM) * (x%NUM))%NUM;
+  
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = ((x%NUM) * (x%NUM))%NUM; // Change x to x^2
+    }
+    return res%NUM;
+}
+
+bool sortbysec(string &a , string &b)
+{
+    /*if(a.first == b.first)
+    {
+        return (a.second <= b.second);
+    }*/
+    return (a[1] <= b[1]) ;
+}
+ 
+ll binarysearch(vector<ll> v, ll find,ll l, ll r)
+{
+    if(find<v[l] or find > v[r]) 
+    {
+        return -1;
+    }
+    while(l<=r)
+    {
+        ll mid  = l + (r-l)/2 ;
+        if(v[mid] == find) return mid;
+        if(v[mid]  > find) r = mid -1;
+        else l =mid +1;
+    }
+    return -1;
+}
+int fact(int x)
+{
+    if(x<=1) return 1;
+    int start =1;
+    int ans =1;
+    while(start<=x)
+    {
+        ans = ans*start;
+        start++;
+    }
+    return ans;
+}
+bool isvalid(int i , int j ,int n,int m,vector<vector<int> > adj)
+{
+
+    if(i<0 or i> n-1 or j<0 or j> m-1)
+    {
+        return false;
+    }
+    if(adj[i][j] == 0) return false;
+    return true;
+}
+void dfs(vector<vector<int> > &adj, vector<vector<bool>> &check, int &ans,int i, int j,int n, int m)
+{
+    if(check[i][j]) return;
+    check[i][j] = true;
+    ans+=adj[i][j] ;
+    if(isvalid(i+1,j,n,m,adj))
+    {
+        dfs(adj,check,ans,i+1,j,n,m);
+    }
+    if(isvalid(i-1,j,n,m,adj))
+    {
+        dfs(adj,check,ans,i-1,j,n,m);
+    }
+    if(isvalid(i,j+1,n,m,adj))
+    {
+        dfs(adj,check,ans,i,j+1,n,m);
+    }
+    if(isvalid(i,j-1,n,m,adj))
+    {
+        dfs(adj,check,ans,i,j-1,n,m);
+    }
+    return;
+}
+vector<ll> factors(ll a, ll b)
+{
+    vector<ll> res;
+    for(ll i=1;i*i<=a;i++)
+    {
+        if(a%i ==0)
+        {
+            res.push_back(i*b);
+            res.push_back((a/i)*b);
+        }
+    }
+    return res;
+}
+bool check(vector<ll> v , ll mid)
+{
+    vector<ll > res;
+    ll val = v[0] - mid;
+    res.push_back(val);
+    for(ll i=1;i< v.size();i++)
+    {
+        if(abs(val - v[i])  > mid)
+        {
+            val = v[i] - mid;
+            res.push_back(val);
+        }
+        else
+        {
+            continue;
+        }
+    }
+    if(res.size() <= 3ll)
+    {
+        return true;
+    }
+    return false;
+
+}
+void solve()
+{
+    int n , q;
+    cin>>n>>q;
+    vector<int> v;
+    for(int i=0;i<n;i++)
+    {
+        int temp;
+        cin>>temp;
+        v.push_back(temp);
+    }
+    vector<int> queries;
+    for(int i=0;i<q;i++)
+    {
+        int temp;
+        cin>>temp;
+        queries.push_back(temp);
+    }
+    map<int,int> mp;
+    for(int i=n-1;i>=0;i--)
+    {
+        mp[v[i]] = i+1;
+    }
+    for(int i=0;i<q;i++)
+    {
+        int num = mp[queries[i]];
+        //
+        cout<<num<<" ";
+        for(auto i: mp)
+        {
+            if(i.second < num)
+            {
+                mp[i.first]++;
+            }
+        }
+        mp[queries[i]] = 1;
+
+    }
+    cout<<endl;
+    return;
+
+}
+
+    
+int main()
+{
+
+    
+    int t ;
+    //cin>>t;
+    t=1;
+    //int i=1;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+    
+    
+}
+
+
+    
